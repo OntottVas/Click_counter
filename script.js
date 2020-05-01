fc2862c288ec83b95964f3755f84e4ca92968b8d
OntottVas <kgergely1607@gmail.com>
2020-05-01 13:45:45 +0200

    Counter works

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-// let clickNumber = ;
+let clickNumber = 0;
 
 $('#increase').click(() => {
+    clickNumber = clickNumber + 1;
     $('h2').text(clickNumber);
+});
+
+$('#decrease').click(() => {
+    if(clickNumber != 0) {
+        clickNumber--;
+        $('h2').text(clickNumber);
+    }
 });
